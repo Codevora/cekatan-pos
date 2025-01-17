17eabdf264267aa9b87d3f3c478ae0738a48bf93
Codevora <codevoratech@gmail.com>
2025-01-18 03:50:04 +0700

    error:No overload matches this call.   Overload 1 of 4, '(args_0: GetServerSidePropsContext): Promise<Session | null>', gave the following error.     Argument of type '(req: NextAuthRequest) => Response | null' is not assignable to parameter of type 'GetServerSidePropsContext'.   Overload 2 of 4, '(args_0: (req: NextAuthRequest, ctx: AppRouteHandlerFnContext) => void | Response | Promise<void | Response>): AppRouteHandlerFn', gave the following error.     Argument of type '(req: NextAuthRequest) => Response | null' is not assignable to parameter of type '(req: NextAuthRequest, ctx: AppRouteHandlerFnContext) => void | Response | Promise<void | Response>'.       Type 'Response | null' is not assignable to type 'void | Response | Promise<void | Response>'.         Type 'null' is not assignable to type 'void | Response | Promise<void |

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,12 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcryptjs";
 
+import { db } from "@/lib/db";
 import {RegisterSchema} from "../schemas";
+import { getUserByEmail } from "../data/user";
+
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
  const validateFields = RegisterSchema.safeParse(values);
@@ -10,6 +14,25 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
  if (!validateFields.success) {
   return {error: "Mohon isi data diri anda!"};
  }
+
+ const { email, password, name } = validateFields.data;
+ const hashedPassword = await bcrypt.hash(password, 10);
+
+ const existingUser = await getUserByEmail(email);
+
+ if (existingUser) {
+  return {error: "Email sudah terdaftar!"};
+ }
+
+ await db.user.create({
+  data: {
+   name,
+   email,
+   password: hashedPassword,
+  },
+ });
+
+ // TODO: send verification token email
 
  return {success: "Berhasil daftar"};
 };
